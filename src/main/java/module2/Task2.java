a4cb1476a7bad8b3f34219094c15a79105cf7d1e
victorialozova <hunter4ik@gmail.com>
2018-03-14 22:08:27 +0200

    Module 2. Task2  By using the bubble sort algorithm, write a Java program to sort an integer array of 10 elements in ascending.

---
+++ src/main/java/module2/Task2.java
@@ -0,0 +1,25 @@
+package module2;
+
+/**
+ * By using the bubble sort algorithm, write a Java program to sort an integer array of 10 elements in ascending.
+ */
+public class Task2 {
+    public static void main(String[] args) {
+        int arr[] = {5, 78, 9, 0, 15, 8, 7, 19, 6, 2};
+        for (int i = 0; i < arr.length - 1; ) {
+            if (arr[i] > arr[i + 1]) {
+                int temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+                if (i != 0) {
+                    i--;
+                }
+            } else {
+                i++;
+            }
+        }
+        for (int j = 0; j < arr.length; j++) {
+            System.out.print(arr[j] + " ");
+        }
+    }
+}
